8672aaabb33cb10ff45907925b6b30d0aefd4156
KaberaSamuel <kaberanshutis@gmail.com>
2025-07-21 17:56:53 +0200

    basic setup of the app, and showing welcoming message on the app start

---
+++ finance-management-app/app.py
@@ -0,0 +1,23 @@
+welcome_message = """--------------------------------------
+    SAVR Financial Management Tool
+--------------------------------------
+Welcome SAVR
+A Management Tool for Small-Scale Entrepreneurs!
+This tool helps you track income, expenses, budgets, and savings goals."""
+
+menu_message = """Please Select an option:
+1. Add Income
+2. Add Expense
+3. Set Budget
+4. Set Savings Goal
+5. View Financial Summary
+6. Exit
+"""
+
+
+def main():
+    print(welcome_message)
+
+
+if __name__ == "__main__":
+    main()
