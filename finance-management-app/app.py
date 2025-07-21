452fbd8d629903e24628b45f51335ad528729a78
KaberaSamuel <kaberanshutis@gmail.com>
2025-07-21 17:58:01 +0200

    added logic for connecting to the database and environment variables handling

--- finance-management-app/app.py
+++ finance-management-app/app.py
@@ -1,3 +1,15 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access an environment variable
+db_host = os.getenv("HOST")
+db_user = os.getenv("USER")
+db_password = os.getenv("PASSWORD")
+
 welcome_message = """--------------------------------------
     SAVR Financial Management Tool
 --------------------------------------
@@ -17,6 +29,14 @@ menu_message = """Please Select an option:
 
 def main():
     print(welcome_message)
+    print("Connecting to the database...")
+    try:
+        mydb = mysql.connector.connect(host=db_host, user=db_user, password=db_password)
+        print("Database connected successfully!")
+    except mysql.connector.Error:
+        print(
+            "Failed to connect to the database, Please check your connection credentials"
+        )
 
 
 if __name__ == "__main__":
